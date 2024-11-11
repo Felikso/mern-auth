25be4128d8a64f6729827bf887cb5b43dfd76308
Ola <xfeliksox@wp.pl>
2024-11-11 08:11:27 +0100

    edit frontend to admin

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,10 @@ app.use('/api/cart',cartRoute)
 app.use('/api/order',orderRoute)
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/dist')));
+	app.use(express.static(path.join(__dirname, '/admin/dist')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'admin', 'dist', 'index.html'));
 	});
 }
 
